27d2f51aefee427fd2133b85669932724c5025b3
Pandabo001 <15621.smnr@gmail.com>
2017-03-16 12:34:10 +0700

    Update ScoreBoard.java

--- CSC1055950/ScoreBoard.java
+++ CSC1055950/ScoreBoard.java
@@ -17,8 +17,8 @@ public class ScoreBoard extends Menu
      * Constructor for objects of class ScoreBoard.
      * 
      */
-    public ScoreBoard()
-    {
+    public ScoreBoard(){
+        
         List<String> entry = new ArrayList<String>();
         int entryCount = 0;
         try{
@@ -30,20 +30,21 @@ public class ScoreBoard extends Menu
                 entryCount++;
             }
             reader.close();
-        }
-        catch(IOException e){
-        }
-        String[] date = new String[entryCount];
-        int[] score = new int[entryCount];
-        int i = 0;
-        while(i < entry.size()){
-            date[i] = entry.get(i).split(" ", 3)[0] + "  " + entry.get(i).split(" ", 3)[1];
-            score[i] = Integer.parseInt(entry.get(i).split(" ", 3)[2]);
-            i++;
+        }catch(IOException e){
+            
         }
         
-        while(i < entryCount){
+        String[] date = new String[entryCount];
+        int[] score = new int[entryCount];
+        for(int i = 0; i < entry.size(); i++){
+            date[i] = entry.get(i).split(" ", 3)[0] + "  " + entry.get(i).split(" ", 3)[1];
+            score[i] = Integer.parseInt(entry.get(i).split(" ", 3)[2]);
+        }
+        
+        for(int i = 0; i < entryCount; i++){
+            
             for(int j = i + 1; j < entryCount; j++){
+                
                 if(score[i] < score[j]){
                     String tmpd = date[i];
                     int tmps = score[i];
@@ -53,8 +54,8 @@ public class ScoreBoard extends Menu
                     score[j] = tmps;
                 }
             }
-            i++;
         }
+        
         GreenfootImage scoreboard = new GreenfootImage("scoreboard.png");
         BG.scale(BG.getWidth() * getHeight() / BG.getHeight(), getHeight());
         getFont();
@@ -63,19 +64,21 @@ public class ScoreBoard extends Menu
         scoreboard.drawString("PLACE", 15, 27);
         scoreboard.drawString("SCORE", 101, 27);
         scoreboard.drawString("DATE AND TIME", 437, 27);
-        while(i < 20){
+        for(int i = 0; i < 20; i++){
+            
             if(i < entryCount){
+                
                 scoreboard.drawString((i + 1) + ".", 15, 57 + i * 22);
                 scoreboard.drawString(score[i] + "", 101, 57 + i * 22);
                 scoreboard.drawString(date[i] + "", 437, 57 + i * 22);
-            }
-            else{
+            }else{
+                
                 scoreboard.drawString((i + 1) + ".", 15, 57 + i * 22);
                 scoreboard.drawString("---", 101, 57 + i * 22);
                 scoreboard.drawString("---", 437, 57 + i * 22);
             }
-            i++;
         }
+        
         BG.drawImage(scoreboard, 0, 0);
         setBackground(BG);
         addObject(new BackButton(), 125, 550);
