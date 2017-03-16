0b1cb9cb6c8662d4308b67a5ac42c1a1c9e1a99c
Pandabo001 <15621.smnr@gmail.com>
2017-03-16 12:40:55 +0700

    Update MyWorld.java

--- CSC1055950/MyWorld.java
+++ CSC1055950/MyWorld.java
@@ -53,39 +53,63 @@ public class MyWorld extends World
      * Spawn the enemy into the World randomly and check the difficulty before spawn enemy ship.
      */
     public void spawn(){
+        
         if(difficulty * 100 + Greenfoot.getRandomNumber(difficulty * 2500) < spawnLocat){
             int spawnCount = 0;
-            
-            spawnCount = spawnLocat / 400;
-            spawnLocat -= spawnCount * 400;
-            int rows = spawnCount / 5;
-            int spawnY = 220 + Greenfoot.getRandomNumber(200);
-            int i = 0;
-            while(i < rows){
-                addObject(new Jet(), 1230 + i * 50, spawnY - 100);
-                addObject(new Jet(), 1230 + i * 50, spawnY - 50);
-                addObject(new Jet(), 1230 + i * 50, spawnY);
-                addObject(new Jet(), 1230 + i * 50, spawnY + 50);
-                addObject(new Jet(), 1230 + i * 50, spawnY + 100);
-                i++;
-            }
-            if(spawnCount % 5 == 1 || spawnCount % 5 == 3){
-                addObject(new Jet(), 1230 + rows * 50, spawnY);
-            }
-            if(spawnCount % 5 == 2 || spawnCount % 5 == 4){
-                addObject(new Jet(), 1230 + rows * 50, spawnY - 25);
-                addObject(new Jet(), 1230 + rows * 50, spawnY + 25);
-            }
-            if(spawnCount % 5 == 3){
-                addObject(new Jet(), 1230 + rows * 50, spawnY - 50);
-                addObject(new Jet(), 1230 + rows * 50, spawnY + 50);                    
-            }
-            if(spawnCount % 5 == 4){
-                addObject(new Jet(), 1230 + rows * 50, spawnY - 75);
-                addObject(new Jet(), 1230 + rows * 50, spawnY + 75);                    
+            if(Greenfoot.getRandomNumber(100) < 15){
+                
+                spawnCount = spawnLocat / 1000;
+                spawnLocat -= spawnCount * 1000;
+                int rows = spawnCount / 3;
+                int spawnY = 190 + Greenfoot.getRandomNumber(260);
+                int i = 0;
+                while(i < rows){
+                    addObject(new Bomber(), 1260 + i * 120, spawnY - 90);
+                    addObject(new Bomber(), 1260 + i * 120, spawnY);
+                    addObject(new Bomber(), 1260 + i * 120, spawnY + 90);
+                    i++;
+                }
+                
+                if(spawnCount % 3 == 1){
+                    addObject(new Bomber(), 1260 + rows * 120, spawnY);
+                }else if(spawnCount % 3 == 2){
+                    addObject(new Bomber(), 1260 + rows * 120, spawnY - 45);
+                    addObject(new Bomber(), 1260 + rows * 120, spawnY + 45);
+                }
+                
+            }else{
+                
+                spawnCount = spawnLocat / 400;
+                spawnLocat -= spawnCount * 400;
+                int rows = spawnCount / 5;
+                int spawnY = 220 + Greenfoot.getRandomNumber(200);
+                int i = 0;
+                while(i < rows){
+                    
+                    addObject(new Jet(), 1230 + i * 50, spawnY - 100);
+                    addObject(new Jet(), 1230 + i * 50, spawnY - 50);
+                    addObject(new Jet(), 1230 + i * 50, spawnY);
+                    addObject(new Jet(), 1230 + i * 50, spawnY + 50);
+                    addObject(new Jet(), 1230 + i * 50, spawnY + 100);  
+                    i++;
+                }
+                if(spawnCount % 5 == 1 || spawnCount % 5 == 3){
+                    addObject(new Jet(), 1230 + rows * 50, spawnY);
+                }
+                if(spawnCount % 5 == 2 || spawnCount % 5 == 4){
+                    addObject(new Jet(), 1230 + rows * 50, spawnY - 25);
+                    addObject(new Jet(), 1230 + rows * 50, spawnY + 25);
+                }
+                if(spawnCount % 5 == 3){
+                    addObject(new Jet(), 1230 + rows * 50, spawnY - 50);
+                    addObject(new Jet(), 1230 + rows * 50, spawnY + 50);                    
+                }
+                if(spawnCount % 5 == 4){
+                    addObject(new Jet(), 1230 + rows * 50, spawnY - 75);
+                    addObject(new Jet(), 1230 + rows * 50, spawnY + 75);                    
+                }
             }
         }
-        
     }
     
     public void act(){
